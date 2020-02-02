dd830298da431ef4acbbd1b03698dd814d9ee27c
raphaellndr <54632013+raphaellndr@users.noreply.github.com>
2020-02-02 11:45:07 +0100

    MainMenu

---
+++ Global Game Jam 2020/Assets/Scripts/GameOverMenu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameOverMenu : MonoBehaviour
+{
+    public Text scoreText;
+    private int score;
+    public Text orderCounterText;
+    public Sprite GoldenStar, EmptyStar;
+    public Image leftStar, middleStar, rightStar;
+
+    void OnEnable()
+    {
+        score = PlayerPrefs.GetInt("CurrentScore1", 0);
+        scoreText.text = score.ToString();
+        orderCounterText.text = "0";
+        if(score<10)
+        {
+            leftStar.sprite = EmptyStar;
+            middleStar.sprite = EmptyStar;
+            rightStar.sprite = EmptyStar;
+        }
+        else if(score<50)
+        {
+            leftStar.sprite = GoldenStar;
+            middleStar.sprite = EmptyStar;
+            rightStar.sprite = EmptyStar;
+        }
+        else if (score < 200)
+        {
+            leftStar.sprite = GoldenStar;
+            middleStar.sprite = GoldenStar;
+            rightStar.sprite = EmptyStar;
+        }
+        else 
+        {
+            leftStar.sprite = GoldenStar;
+            middleStar.sprite = GoldenStar;
+            rightStar.sprite = GoldenStar;
+        }
+    }
+}
