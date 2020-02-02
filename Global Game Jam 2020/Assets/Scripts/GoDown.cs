30fb3a7b6ce5a2c832c2f9515f892abced69fdae
OscarAFK <57917670+OscarAFK@users.noreply.github.com>
2020-02-02 11:59:35 +0100

    amelioration teleporteurs

--- Global Game Jam 2020/Assets/Scripts/GoDown.cs
+++ Global Game Jam 2020/Assets/Scripts/GoDown.cs
@@ -4,20 +4,34 @@ using UnityEngine;
 
 public class GoDown : MonoBehaviour
 {
-    Vector2 downPosition;
-    public GameObject down;
+    Vector2 upPosition;
+    public GameObject up;
 
     // Start is called before the first frame update
     void Start()
     {
-        downPosition = down.transform.position;
+        upPosition = up.transform.position;
     }
 
-    void OnTriggerStay2D(Collider2D collider)
+    IEnumerator detectKey(GameObject player)
     {
-        if (Input.GetKeyDown(KeyCode.E) && collider.gameObject.tag == "Player")
+        while (1 == 1)
         {
-            collider.transform.position = downPosition - Vector2.up;
+            if (Input.GetKeyDown(KeyCode.E) && player.tag == "Player")
+            {
+                player.transform.position = upPosition - Vector2.up;
+            }
+            yield return new WaitForSeconds(0.0005f);
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine("detectKey", collision.gameObject);
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StopCoroutine("detectKey");
     }
 }
